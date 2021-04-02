58a3c5dffe93e1c790bd712b819184607d08e01c
Marquis520 <Marquis520@icloud.com>
2021-04-01 21:54:36 -0400

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Your code here
+function saturdayFun(a = "roller-skate"){
+    return `This Saturday, I want to ${a}!`
+}
+
+function mondayWork(b = "go to the office"){
+    return `This Monday, I will ${b}.`
+}
+
+function wrapAdjective(c = "*"){
+    return function(d = "special"){
+        return `You are ${c}${d}${c}!`
+    }
+}
+
+let Calculator = {
+    add: function (e , f){
+        return e + f;
+    } ,
+    subtract: function (e , f){
+        return e - f;
+    } ,
+    multiply: function (e , f){
+        return e * f;
+    } ,
+    divide: function (e , f){
+        return e / f;
+    }
+}
+
+function actionApplyer(start, array){
+    let h = start
+        for (let i = 0; i < array.length; i++){
+            h = array[i](h)
+        }
+        return h
+}
